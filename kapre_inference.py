4d7ed6a90f298b36b64497097401ba02f2ca2de1
ow4g18 <52359446+ow4g18@users.noreply.github.com>
2020-09-02 14:38:01 +0100

    Update kapre_inference.py

--- kapre_inference.py
+++ kapre_inference.py
@@ -60,7 +60,7 @@ model = tf.keras.models.load_model(wd)
 
 tfrecords = [os.path.join(wd, '00000_00256.tfrecord')]
 
-test_audio = get_test_dataset(tfrecords, batch_size)
+test_audio = get_test_dataset(tfrecords, batch_size=batch_size)
 
-print(model.evaluate(test_audio, batch_size))
+print(model.evaluate(test_audio, batch_size=batch_size))
 
